ac6c57dd3862fda7766f97927e01946444a02687
bkim1999 <bumcheol.kim.1999@gmail.com>
2024-05-28 18:29:22 +0900

    :rocket: Deploy Test

--- src/main/java/com/eatfluencer/eatfluencer_api/controller/MvcController.java
+++ src/main/java/com/eatfluencer/eatfluencer_api/controller/MvcController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MvcController {
 	@GetMapping("/test")
     public String test(){
-        String testStr = "Hi~~";
+        String testStr = "TEST";
         System.out.println(testStr);
         return testStr;
     }
